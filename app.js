61f970cb34a0630a5232ddd92d3deb263cbe2a11
kadir-zeren <k4dzeren@gmail.com>
2023-12-14 18:24:09 +0300

    selectors

---
+++ app.js
@@ -0,0 +1,19 @@
+//* ------ Selectors ------- */
+const selectionArticle = document.querySelector(".selection");
+
+//? Secilen elemanlarin tayicilari
+const yourChoiceDiv = document.getElementById("your-choice");
+const pcChoiceDiv = document.getElementById("pc-choice");
+
+//? message
+const messagePar = document.querySelector(".message");
+
+//? Score
+const scoreCardSection = document.querySelector(".score-card");
+const pcScoreSpan = document.getElementById("pc-score");
+const yourScoreSpan = document.getElementById("your-score");
+
+//? Modal
+const modalCardSection = document.querySelector(".modal-card");
+const finalMessagePar = document.getElementById("final-message");
+const playAgainBtn = document.getElementById("play-again");
